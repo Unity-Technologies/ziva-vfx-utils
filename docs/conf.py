074660707a71bb38d983a2fdaef703ad6a39bd83
Lonnie Kraatz <lonniek@zivadynamics.com>
2017-03-24 10:49:44 -0700

    doc update

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'zBuilder.tex', u'zBuilder Documentation',
-     u'Lonnie Kraatz', 'manual'),
+     u'Ziva Dynamics', 'manual'),
 ]
 
 
