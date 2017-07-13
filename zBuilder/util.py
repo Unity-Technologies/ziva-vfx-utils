93039d1a1673b32bea59410d1e7c5d73536e4103
Lonnie Kraatz <lonniek@zivadynamics.com>
2017-07-13 11:21:58 -0700

    fix for util.copy()
    it was creating a short name from long name incorrectly.

--- zBuilder/util.py
+++ zBuilder/util.py
@@ -8,7 +8,7 @@ def copy_paste(*args,**kwargs):
     A utility wrapper for copying and pasting a tissue
     '''
     sel = mc.ls(sl=True)
-
+    #print sel
 
     selection = None
     if args:
@@ -16,12 +16,14 @@ def copy_paste(*args,**kwargs):
     else:
         selection = mc.ls(sl=True,l=True)
 
-
     import zBuilder.setup.Ziva as zva
     z = zva.ZivaSetup()
     z.retrieve_from_scene_selection(selection[0])
-    z.string_replace(selection[0].replace('|',''), selection[1].replace('|',''))
+    z.string_replace(selection[0].split('|')[-1], selection[1].split('|')[-1])
     z.stats()
     z.apply(kwargs)
 
     mc.select(sel)
+
+
+
