f7f114066f16583a1e31cfd8099d37f7729d21da
Lonnie Kraatz <lonniek@zivadynamics.com>
2017-11-10 13:24:27 -0800

    test for copying

--- zBuilder/tests/util_copy_paste.py
+++ zBuilder/tests/util_copy_paste.py
@@ -1,23 +1,21 @@
 # this removes any mention of zBuilder in sys.modules so you get a clean import
 import zBuilder.tests.utils as utl
+
 utl.hard_reload('zBuilder')
 
 import maya.cmds as mc
 import zBuilder.zMaya as mz
 import zBuilder.builders.ziva as zva
 
-
 # This builds the Zivas anatomical arm demo with no pop up dialog.
 utl.build_arm()
 
 mc.select(cl=True)
 
-# use builder to retrieve from scene--------------------------------------------
-z = zva.Ziva()
-z.retrieve_from_scene()
+mc.duplicate('r_bicep_muscle', name='dupe')
+mc.polySmooth('dupe')
+mc.select('r_bicep_muscle', 'dupe')
 
-# remove ziva nodes from scene so all we have left is geo
-mz.clean_scene()
+import zBuilder.util as utl
 
-# build
-z.build(check_meshes=False)
+utl.copy_paste()
