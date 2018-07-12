74e92801791c8cd11dd60449183f8de52c91e6f1
Lonnie Kraatz <lonniek@zivadynamics.com>
2018-07-11 21:20:16 -0700

    Getting connected to selected instead of whole scene to speed it up

--- zUI/ui.py
+++ zUI/ui.py
@@ -7,7 +7,7 @@ class ZivaUi():
         z = zva.Ziva()
         # Not getting the parameters in this case (maps and meshes) as that
         # slows down the retrieving of information.
-        z.retrieve_from_scene(get_parameters=False)
+        z.retrieve_connections()
         z.view()
 
 
