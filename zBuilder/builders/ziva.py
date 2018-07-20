c7ba8cc6afd0db53a6722e08a77117ab30df768a
Lonnie Kraatz <lonniek@zivadynamics.com>
2018-07-20 12:53:34 -0700

    Check to see if there are existing Ziva nodes in scene before
    launching UI.

--- zBuilder/builders/ziva.py
+++ zBuilder/builders/ziva.py
@@ -133,8 +133,9 @@ class Ziva(Builder):
         target = mm.eval('zQuery -at')
         mc.select(source,target)
         nodes = mm.eval('zQuery -a')
-        self._populate_nodes(nodes, get_parameters=get_parameters)
-        self.get_parent()
+        if nodes:
+            self._populate_nodes(nodes, get_parameters=get_parameters)
+            self.get_parent()
 
         mc.select(scene_selection)
 
