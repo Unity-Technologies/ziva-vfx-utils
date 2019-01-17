f428c960363783d375d775e2cabfeebc53cdce26
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-01-17 11:37:48 -0800

    removing unused files

--- zBuilder/tests/CmtTests/run_vfx_smoke_tests.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-: default maya version is 2018 unless overriden by a parameter
-if "%~1"=="" (
-    set mayaYear=2018
-) else (
-    set mayaYear=%1
-)
-
-python %~dp0..\..\..\CMT\bin\runmayatests.py -m %mayaYear% --path %~dp0%tests
--- zBuilder/tests/CmtTests/run_vfx_smoke_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-MayaYear="2014"
-if [ ! -z "$1" ] 
-    then MayaYear=$1 # if argument supplied - treat it as a maya year
-fi
-export zivadyn_LICENSE="/var/ziva/ziva_internal_use.lic"
-python $ScriptDir/../../../CMT/bin/runmayatests.py --maya $MayaYear --path ${ScriptDir}/tests
-
