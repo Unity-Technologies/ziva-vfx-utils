0a66aca81f2fa8b9cf959c3276eecac3bb24f11f
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-06-25 12:04:15 -0700

    building multiple zCloth at a time.
    Now this aligns how all the bodies are being built (zBone, zTissue
    and zCloth).  This also fixes a bug exclusive to zCloth.  VFXACT-120

--- zBuilder/nodes/ziva/zCloth.py
+++ zBuilder/nodes/ziva/zCloth.py
@@ -28,20 +28,53 @@ class ClothNode(Ziva):
                 tmp = {'zSolver':['substeps']}
         """
         attr_filter = kwargs.get('attr_filter', list())
+        name_filter = kwargs.get('name_filter', list())
+        permissive = kwargs.get('permissive', True)
 
-        name = self.name
+        parameters = self.builder.bundle.get_scene_items(type_filter='zCloth',
+                                                         name_filter=name_filter)
 
-        if not mc.objExists(name):
+        # checking if the node is the first one in list.  If it is I get
+        # all the zCloth and build them together for speed reasons.
+        # This feels kinda sloppy to me.
 
-            Ziva.check_meshes(self.association)
+        if self == parameters[0]:
+            build_multiple(parameters, attr_filter=attr_filter, permissive=permissive)
 
-            mc.select(self.association)
-            results = mm.eval('ziva -c')
-            cloth = mc.ls(results, type='zCloth')[0]
-            mc.rename(cloth, name)
-            self.mobject = name
-        else:
-            new_name = mc.rename(self.get_scene_name(), self.name)
-            self.mobject = new_name
 
-        self.set_maya_attrs(attr_filter=attr_filter)
+def build_multiple(parameters, attr_filter=None, permissive=False):
+    """ Each node can deal with it's own building.  Though, with zCLoth it is much
+    faster to build them all at once with one command instead of looping
+    through them.  This function builds all the zCloth at once.
+
+    Args:
+        permissive (bool):
+        parameters:
+        attr_filter (obj):
+
+    Returns:
+
+    """
+    sel = mc.ls(sl=True)
+    # cull none buildable------------------------------------------------------
+    culled = mz.cull_creation_nodes(parameters)
+
+    # build bones all at once--------------------------------------------------
+    results = None
+    if culled['meshes']:
+        Ziva.check_meshes(culled['meshes'])
+        mc.select(culled['meshes'], r=True)
+        results = mm.eval('ziva -c')
+
+    # rename zCloth------------------------------------------------------------
+    if results:
+        results = mc.ls(results, type='zCloth')
+        for new, name, parameter in zip(results, culled['names'], culled['parameters']):
+            parameter.mobject = new
+            mc.rename(new, name)
+
+    # set the attributes
+    for parameter in parameters:
+        parameter.set_maya_attrs(attr_filter=attr_filter)
+
+    mc.select(sel)
