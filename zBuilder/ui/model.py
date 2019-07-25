fc154cbd0c9d6ebf9e2c63c72d7ee4344dedfd26
sega_moon <stanp@zivadynamics.com>
2019-07-24 19:47:21 -0700

    added rename option to treeview by double clicking on item

--- zBuilder/ui/model.py
+++ zBuilder/ui/model.py
@@ -1,5 +1,6 @@
 from PySide2 import QtGui, QtWidgets, QtCore
 from icons import get_icon_path_from_node
+import maya.cmds as mc
 
 
 class SceneGraphModel(QtCore.QAbstractItemModel):
@@ -31,11 +32,19 @@ class SceneGraphModel(QtCore.QAbstractItemModel):
         return 1
 
     def flags(self, index):
-        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
 
     def headerData(self, section, orientation, role):
         if role == QtCore.Qt.DisplayRole:
             return "Scene Items"
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if role == QtCore.Qt.EditRole:
+            node = index.internalPointer()
+            long_name = node.long_name
+            mc.rename(long_name, value)
+            node.name = value
+        super(SceneGraphModel, self).setData(index, value, role)
 
     def data(self, index, role):
 
