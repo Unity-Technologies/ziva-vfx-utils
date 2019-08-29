1e7ddecae4a48202cae89617a23e53c2d26576a7
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-08-29 14:31:57 -0700

    VFXACT-149 Adding skeleton node for zRestShape

---
+++ zBuilder/nodes/ziva/zRestShape.py
@@ -0,0 +1,17 @@
+from zBuilder.nodes import Ziva
+import maya.cmds as mc
+import maya.mel as mm
+import zBuilder.zMaya as mz
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class RestShapeNode(Ziva):
+    """ This node for storing information related to zFibers.
+    """
+    type = 'zRestShape'
+    """ The type of node. """
+
+    def __init__(self, *args, **kwargs):
+        Ziva.__init__(self, *args, **kwargs)
