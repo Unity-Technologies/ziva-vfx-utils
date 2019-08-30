958e3dc2031be3a53b949bcede804c0f58550637
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-08-30 15:41:45 -0700

    VFXACT-149 Storing the tissue name and targets long name restShape

--- zBuilder/nodes/ziva/zRestShape.py
+++ zBuilder/nodes/ziva/zRestShape.py
@@ -26,8 +26,16 @@ class RestShapeNode(Ziva):
         super(RestShapeNode, self).populate(maya_node=maya_node)
 
         self.targets = mc.listConnections(self.name + '.target')
+        self.targets = mc.ls(self.targets, long=True)  # find long names
+        self.tissue_name = get_rest_shape_tissue(self.name)
 
     def build(self, *args, **kwargs):
         """ Builds the node in maya.  mean to be overwritten.
         """
         pass
+
+
+def get_rest_shape_tissue(rest_shape):
+    tet = mc.listConnections('{}.iGeo'.format(rest_shape))[0]
+    tissue = mm.eval('zQuery -type "zTissue" {}'.format(tet))
+    return tissue[0]
