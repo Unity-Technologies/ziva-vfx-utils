13ac5407d605743b013671e4f7bfe4aa8b719db8
sega_moon <stanp@zivadynamics.com>
2019-09-04 10:00:44 -0700

    removed unrelated staff and merged with release/version1.7

--- zBuilder/ui/view.py
+++ zBuilder/ui/view.py
@@ -1,5 +1,69 @@
 from PySide2 import QtGui, QtWidgets, QtCore
 import icons
+import maya.mel as mm
+
+
+class GroupedLineEdit(QtWidgets.QLineEdit):
+    """
+    Groups LineEdits together so after you press Tab it switch focus to sibling
+    Sends acceptSignal when Enter or Return buttons pressed
+    """
+    acceptSignal = QtCore.Signal()
+
+    def __init__(self, parent=None):
+        super(GroupedLineEdit, self).__init__(parent)
+        self.sibling = None
+
+    def event(self, event):
+        if event.type() == QtCore.QEvent.KeyPress and event.key() == QtCore.Qt.Key_Tab:
+            if self.sibling:
+                self.sibling.setFocus()
+                return True
+
+        if event.type() == QtCore.QEvent.KeyPress and event.key() in [
+                QtCore.Qt.Key_Enter, QtCore.Qt.Key_Return
+        ]:
+            self.acceptSignal.emit()
+            return True
+
+        return super(GroupedLineEdit, self).event(event)
+
+
+class ProximityWidget(QtWidgets.QWidget):
+    """
+    Widget in right-click menu to change map weights for attachments
+    """
+
+    def __init__(self, parent=None):
+        super(ProximityWidget, self).__init__(parent)
+        h_layout = QtWidgets.QHBoxLayout(self)
+        h_layout.setContentsMargins(15, 15, 15, 15)
+        self.from_edit = GroupedLineEdit()
+        self.from_edit.setFixedHeight(24)
+        self.from_edit.setPlaceholderText("From")
+        self.from_edit.setText("0.1")
+        self.from_edit.setFixedWidth(40)
+        self.to_edit = GroupedLineEdit()
+        self.to_edit.setFixedHeight(24)
+        self.to_edit.setPlaceholderText("To")
+        self.to_edit.setText("0.2")
+        self.to_edit.setFixedWidth(40)
+        self.from_edit.sibling = self.to_edit
+        self.to_edit.sibling = self.from_edit
+        ok_button = QtWidgets.QPushButton()
+        ok_button.setText("Ok")
+        h_layout.addWidget(self.from_edit)
+        h_layout.addWidget(self.to_edit)
+        h_layout.addWidget(ok_button)
+        ok_button.clicked.connect(self.paintByProx)
+        self.from_edit.acceptSignal.connect(self.paintByProx)
+        self.to_edit.acceptSignal.connect(self.paintByProx)
+
+    def paintByProx(self):
+        """Paints attachment map by proximity.
+        """
+        mm.eval('zPaintAttachmentsByProximity -min {} -max {}'.format(self.from_edit.text(),
+                                                                      self.to_edit.text()))
 
 
 class SceneTreeView(QtWidgets.QTreeView):
@@ -20,11 +84,13 @@ class SceneTreeView(QtWidgets.QTreeView):
         row_count = self.model().rowCount(index.parent())
 
         for column in xrange(column_count):
-            x_1 = column * self.indentation()
-            x_2 = self.indentation()
-
-            rect = QtCore.QRect(x_1 + 20, rect.top(), x_2,
-                                rect.height())
+            # padding from the left side of widget
+            offset = 20
+            pos_x = column * self.indentation()
+            pos_y = rect.top()
+            width = self.indentation()
+            height = rect.height()
+            rect = QtCore.QRect(pos_x + offset, pos_y, width, height)
             if column == column_count - 1:
                 if node.child_count():
                     if self.isExpanded(index):
