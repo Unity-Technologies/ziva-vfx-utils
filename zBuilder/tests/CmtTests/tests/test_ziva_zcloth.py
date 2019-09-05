81fc30f6bcc3fbb5a5a051b9bbb4a2689a61e0e6
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-09-05 12:38:51 -0700

    VFXACT-218 Rename util.py to utils.py

--- zBuilder/tests/CmtTests/tests/test_ziva_zcloth.py
+++ zBuilder/tests/CmtTests/tests/test_ziva_zcloth.py
@@ -7,7 +7,7 @@ import sys
 import zBuilder.zMaya as mz
 import zBuilder.builders.ziva as zva
 import zBuilder.tests.utils as utl
-import zBuilder.util as utility
+import zBuilder.utils as utility
 
 from vfx_test_case import VfxTestCase
 
