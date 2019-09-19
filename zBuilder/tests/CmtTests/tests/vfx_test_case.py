fcf82af619f209eb41a7ecacd045923c5b81e3b2
Essex Edwards <essexe@zivadynamics.com>
2019-09-19 14:21:59 -0700

    VFXACT-313 Add some mesh comparison utilities

--- zBuilder/tests/CmtTests/tests/vfx_test_case.py
+++ zBuilder/tests/CmtTests/tests/vfx_test_case.py
@@ -2,6 +2,19 @@ import os
 import maya.cmds as cmds
 from cmt.test import TestCase
 
+def isApprox(a, b, eps=1e-6):
+    if hasattr(type(a), '__iter__'):
+        if len(a) != len(b):
+            return False
+        return all([isApprox(ai, bi, eps) for ai, bi in zip(a, b)])
+    else:
+        return abs(a - b) <= eps
+
+def get_mesh_vertex_positions(mesh):
+    """ Given the name of a mesh, return a flat list of its world-space vertex positions."""
+    # See comments here: http://www.fevrierdorian.com/blog/post/2011/09/27/Quickly-retrieve-vertex-positions-of-a-Maya-mesh-%28English-Translation%29
+    return cmds.xform(mesh+'.vtx[*]', q=True, ws=True, t=True)
+
 class VfxTestCase(TestCase):
     """Base class for unit test cases run for ZivaVFX plugin."""
 
@@ -33,6 +46,14 @@ class VfxTestCase(TestCase):
         # all this negation is to make sure that NaN fails.
         if not (a>=b-eps) or not (a<=b+eps):
             raise AssertionError("{} and {} are not approximately equal, with tolerance {}".format(a,b,eps))
+
+    def assertAllApproxEqual(self, a, b, eps=1e-6):
+        """Fail iff |a[i]-b[i]|>eps for all i"""
+        if len(a) != len(b):
+            raise AssertionError("{} and {} are not approximately equal, with tolerance {}".format(
+                a, b, eps))
+        for ai, bi in zip(a, b):
+            self.assertApproxEqual(ai, bi, eps)
 
 
 def get_plugin_path():
