c595b7a8db16886cbc81322c6cb6288e3ff5a156
SEGA <stanp@zivadynamics.com>
2019-10-22 14:31:02 -0700

    VFXACT-387 refactoring

--- tests/CmtTests/tests/test_zbuilder_nodes_zsolver.py
+++ tests/CmtTests/tests/test_zbuilder_nodes_zsolver.py
@@ -32,12 +32,12 @@ class ZivaSolverGenericTestCase(VfxTestCase):
         if os.path.exists(self.temp_file_path):
             os.remove(self.temp_file_path)
 
-    def check_retrieve_zsolver_looks_good(self, builder, name, attrs=None):
+    def check_retrieve_zsolver_looks_good(self, builder, name, attrs):
         """Args:
             builder (builders.ziva.Ziva()): builder object
             name (string): name of the solver transform to check
             attrs (list): compares to stored zBuilder values for zSolver
-                          if not defined - getting values from the scene
+                          if empty - getting values from the scene
         """
         # get solver from zBuilder
         solver_nodes = builder.get_scene_items(type_filter='zSolver')
@@ -61,12 +61,12 @@ class ZivaSolverGenericTestCase(VfxTestCase):
                 value = mc.getAttr("{}.{}".format(solver.name, attr))
             self.assertTrue(value == solver.attrs[attr]['value'])
 
-    def check_retrieve_zsolver_transform_looks_good(self, builder, name, attrs=None):
+    def check_retrieve_zsolver_transform_looks_good(self, builder, name, attrs):
         """Args:
             builder (builders.ziva.Ziva()): builder object
             name (string): name of the solver transform to check
             attrs (list): compares to stored zBuilder values for zSolverTransform
-                          if not defined - getting values from the scene
+                          if empty - getting values from the scene
         """
         # get solver transform from zBuilder
         solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
@@ -102,31 +102,34 @@ class ZivaSolverGenericTestCase(VfxTestCase):
                                 solver_transform_children_expected)
 
     def test_retrieve(self):
-        self.check_retrieve_zsolver_looks_good(self.builder, "zSolver1Shape")
-        self.check_retrieve_zsolver_transform_looks_good(self.builder, "zSolver1")
+        self.check_retrieve_zsolver_looks_good(self.builder, "zSolver1Shape", [])
+        self.check_retrieve_zsolver_transform_looks_good(self.builder, "zSolver1", [])
+
+    def check_solver_and_transform_looks_good(self, builder,
+                                              solver_name,
+                                              solver_transform_name):
+        solver_nodes = builder.get_scene_items(type_filter='zSolver')
+        self.assertEqual(len(solver_nodes), 1)
+
+        solver = solver_nodes[0]
+        self.assertEqual(solver.name, solver_name)
+        self.assertEqual(solver.type, "zSolver")
+
+        solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
+        self.assertEqual(len(solver_transform_nodes), 1)
+
+        solver_transform = solver_transform_nodes[0]
+        self.assertEqual(solver_transform.name, solver_transform_name)
+        self.assertEqual(solver_transform.type, "zSolverTransform")
 
     def test_builder_has_same_solver_node_after_roundtrip_to_disk(self):
         self.builder.write(self.temp_file_path)
-
         self.assertTrue(os.path.exists(self.temp_file_path))
 
         builder = zva.Ziva()
         builder.retrieve_from_file(self.temp_file_path)
 
-        solver_nodes = builder.get_scene_items(type_filter='zSolver')
-
-        self.assertEqual(len(solver_nodes), 1)
-
-        solver = solver_nodes[0]
-        solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
-
-        solver_transform = solver_transform_nodes[0]
-
-        self.assertEqual(solver.name, "zSolver1Shape")
-        self.assertEqual(solver.type, "zSolver")
-
-        self.assertEqual(solver_transform.name, "zSolver1")
-        self.assertEqual(solver_transform.type, "zSolverTransform")
+        self.check_solver_and_transform_looks_good(builder, "zSolver1", "zSolver1Transform")
 
     def test_build(self):
         solver_attrs = ['substeps',
@@ -163,20 +166,7 @@ class ZivaSolverGenericTestCase(VfxTestCase):
         mz.clean_scene()
         builder.build()
 
-        solver_nodes = builder.get_scene_items(type_filter='zSolver')
-
-        self.assertEqual(len(solver_nodes), 1)
-
-        solver = solver_nodes[0]
-        solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
-
-        solver_transform = solver_transform_nodes[0]
-
-        self.assertEqual(solver.name, "zSolver1Shape")
-        self.assertEqual(solver.type, "zSolver")
-
-        self.assertEqual(solver_transform.name, "zSolver1")
-        self.assertEqual(solver_transform.type, "zSolverTransform")
+        self.check_solver_and_transform_looks_good(builder, "zSolver1", "zSolver1Transform")
 
 
 class ZivaSolverTestCase(VfxTestCase):
