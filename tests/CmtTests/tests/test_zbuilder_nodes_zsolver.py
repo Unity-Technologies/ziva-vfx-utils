e5ec83faa705604d142024c8f343c66eec2dceac
SEGA <stanp@zivadynamics.com>
2019-10-21 17:02:18 -0700

    VFXACT-387 added check_solver_name_and_type method for better readability

--- tests/CmtTests/tests/test_zbuilder_nodes_zsolver.py
+++ tests/CmtTests/tests/test_zbuilder_nodes_zsolver.py
@@ -98,6 +98,17 @@ class ZivaSolverGenericTestCase(VfxTestCase):
         self.assertGreaterEqual(solver_transform_children,
                                 solver_transform_children_expected)
 
+    def check_solver_name_and_type(self, builder, solver):
+        solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
+
+        solver_transform = solver_transform_nodes[0]
+
+        self.assertEqual(solver.name, "zSolver1Shape")
+        self.assertEqual(solver.type, "zSolver")
+
+        self.assertEqual(solver_transform.name, "zSolver1")
+        self.assertEqual(solver_transform.type, "zSolverTransform")
+
     def test_retrieve(self):
         self.check_retrieve_zsolver_looks_good(self.builder)
         self.check_retrieve_zsolver_transform_looks_good(self.builder)
@@ -114,16 +125,7 @@ class ZivaSolverGenericTestCase(VfxTestCase):
 
         self.assertEqual(len(solver_nodes), 1)
 
-        solver = solver_nodes[0]
-        solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
-
-        solver_transform = solver_transform_nodes[0]
-
-        self.assertEqual(solver.name, "zSolver1Shape")
-        self.assertEqual(solver.type, "zSolver")
-
-        self.assertEqual(solver_transform.name, "zSolver1")
-        self.assertEqual(solver_transform.type, "zSolverTransform")
+        self.check_solver_name_and_type(builder, solver_nodes[0])
 
     def test_build_with_one_solver(self):
         solver_attrs = ['substeps',
@@ -162,16 +164,7 @@ class ZivaSolverGenericTestCase(VfxTestCase):
 
         self.assertEqual(len(solver_nodes), 1)
 
-        solver = solver_nodes[0]
-        solver_transform_nodes = builder.get_scene_items(type_filter='zSolverTransform')
-
-        solver_transform = solver_transform_nodes[0]
-
-        self.assertEqual(solver.name, "zSolver1Shape")
-        self.assertEqual(solver.type, "zSolver")
-
-        self.assertEqual(solver_transform.name, "zSolver1")
-        self.assertEqual(solver_transform.type, "zSolverTransform")
+        self.check_solver_name_and_type(builder, solver_nodes[0])
 
 
 class ZivaSolverTestCase(VfxTestCase):
