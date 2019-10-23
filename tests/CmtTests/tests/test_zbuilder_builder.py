923ae87c8bf88f86303f8b2aa3b519211bed27e8
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-10-23 15:41:56 -0700

    VFXACT-390 Adding tests for comparing zBuilder objects

---
+++ tests/CmtTests/tests/test_zbuilder_builder.py
@@ -0,0 +1,47 @@
+import os
+import tempfile
+
+import maya.cmds as mc
+import maya.mel as mm
+
+import zBuilder.builders.ziva as zva
+from vfx_test_case import VfxTestCase
+import tests.utils as test_utils
+
+
+class ZivaBuilderTestCase(VfxTestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_builder_compare(self):
+        """ Getting the generic scene and writing out a zBuilder file.  This will
+        allow us to retrieve file and copmpare it against scene.
+
+        We are going to compare builder from this setUp and a builder retrieved from 
+        the file.
+        """
+        test_utils.build_generic_scene()
+        fd, file_name = tempfile.mkstemp(suffix='.zBuilder')
+        os.close(fd)
+
+        mc.select('zSolver1')
+        builder_orig = zva.Ziva()
+        builder_orig.retrieve_from_scene()
+        builder_orig.write(file_name)
+        # compare against this one
+        mc.select('zSolver1')
+        builder_from_file = zva.Ziva()
+        builder_from_file.retrieve_from_file(file_name)
+        # The bundles should be same
+        self.assertTrue(builder_orig == builder_from_file)
+
+        # change an item in the builder, lets compare.  should be not equal
+        # in this case changing an attribute value.
+        builder_from_file.get_scene_items(
+            name_filter='c_tissue_3_zMaterial')[0].attrs['massDensity']['value'] = 1070.0
+
+        self.assertFalse(builder_orig == builder_from_file)
---
+++ tests/CmtTests/tests/test_zbuilder_nodes_base.py
@@ -0,0 +1,49 @@
+import os
+import tempfile
+
+import maya.cmds as mc
+import maya.mel as mm
+
+import zBuilder.builders.ziva as zva
+from vfx_test_case import VfxTestCase
+import tests.utils as test_utils
+
+
+class ZivaBaseTestCase(VfxTestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_node_compares(self):
+        """ Getting the generic scene and writing out a zBuilder file.  This will
+        allow us to retrieve file and copmpare it against scene.
+
+        We are going to compare builder from this setUp and a builder retrieved from 
+        the file.
+        """
+        test_utils.build_generic_scene()
+        fd, file_name = tempfile.mkstemp(suffix='.zBuilder')
+        os.close(fd)
+
+        mc.select('zSolver1')
+        builder_orig = zva.Ziva()
+        builder_orig.retrieve_from_scene()
+        builder_orig.write(file_name)
+        # compare against this one
+        mc.select('zSolver1')
+        builder_from_file = zva.Ziva()
+        builder_from_file.retrieve_from_file(file_name)
+
+        # get item from eachbuilder
+        item1 = builder_orig.get_scene_items(type_filter='zMaterial')[0]
+        item2 = builder_from_file.get_scene_items(type_filter='zMaterial')[0]
+
+        # should be same
+        self.assertTrue(item1 == item2)
+
+        # change a value in one and compare
+        item1.attrs['massDensity']['value'] = 70.0
+        self.assertFalse(item1 == item2)
