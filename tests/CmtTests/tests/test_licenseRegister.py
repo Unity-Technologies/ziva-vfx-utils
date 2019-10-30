731ce88ca4147bc7dd91498ddcac8c12089244c6
Chengdong Gui <chengdongg@zivadynamics.com>
2019-10-30 12:37:47 -0700

    VFXACT-403 Refactoring code from class to functions
    After watching this talk: https://www.youtube.com/watch?v=o9pEzgHorH0,
    I realize current class style implementation is over designed and too
    complex for such simple task.
    
    This commit changes to 2 public functions, register_node_based_license()
    and register_floating_license().
    
    All the requested parameter directly pass into these 2 functions.
    Detail logic, such as read/write file, are in the private functions.
    
    After refactoring, code size decreased and unnecessary test cases are
    removed.

--- tests/CmtTests/tests/test_licenseRegister.py
+++ tests/CmtTests/tests/test_licenseRegister.py
@@ -1,10 +1,9 @@
 import unittest
-import os
-from vfx_test_case import VfxTestCase
-from os import path
-import re
+from os import path, mkdir
 import tempfile
-import utils.licenseRegister.licenseRegister as licReg
+from vfx_test_case import VfxTestCase
+from utils.licenseRegister.licenseRegister import register_node_based_license, register_floating_license
+
 
 class TestLicenseRegister(VfxTestCase):
   
@@ -15,16 +14,10 @@ class TestLicenseRegister(VfxTestCase):
         self.temp_dir = tempfile.gettempdir()
         self.module_path = self.temp_dir + '/Ziva/VFX/Ziva-VFX-Maya-Module'
         try:
-            os.mkdir(self.module_path)
+            mkdir(self.module_path)
         except OSError:
             pass  # path exists, proceed
         
-        self.licReg = licReg.LicenseRegister(self.module_path)
-
-    def test_register_without_set_license_type(self):
-        with self.assertRaises(RuntimeError):
-            self.licReg.register()
-
 
     def test_register_node_based_license(self):
         '''
@@ -36,28 +29,26 @@ LICENSE zivadyn ziva-vfx-author 1.99 1-jan-2020 uncounted hostid=ANY
   _ck=abcde12345 sig="AABBCCDDEEFF112233445566AABBCCDDEEFF112233445566
   ABCDEF123456ABCDEF123456ABCDEF123456"
 '''.strip()
-        new_node_based_license_file_path = \
-            '{}/new_node_based_license.lic'.format(self.temp_dir)
+        new_node_based_license_file_path = path.normpath(self.temp_dir + '/new_node_based_license.lic')
         with open(new_node_based_license_file_path, 'w') as input_file:
             input_file.write(new_node_based_license_content)
             
         # Act
-        self.licReg.set_node_based_license_info(new_node_based_license_file_path)
-        self.licReg.register()
+        register_node_based_license(self.module_path, new_node_based_license_file_path)
 
         # Verify
         file_path = '{}/zivavfx.lic'.format(self.module_path)
-        self.assertTrue(os.path.exists(file_path))
-        self.assertTrue(os.path.isfile(file_path))
+        self.assertTrue(path.exists(file_path))
+        self.assertTrue(path.isfile(file_path))
         with open(file_path, 'r') as lic_file:
             generated_content = lic_file.read()
             self.assertEqual(new_node_based_license_content, generated_content)
 
+
     def test_non_exist_input_node_based_file(self):
         # Act
-        self.licReg.set_node_based_license_info("non/exist/file/path")
         with self.assertRaises(RuntimeError):
-            self.licReg.register()
+            register_node_based_license(self.module_path, "non/exist/file/path")
 
 
     def test_register_floating_license(self):
@@ -65,29 +56,16 @@ LICENSE zivadyn ziva-vfx-author 1.99 1-jan-2020 uncounted hostid=ANY
         Test registering a floating license, no existing license file
         '''
         # Setup
-        new_floating_license_server_info = {
-          'SERVER' : 'localhost',
-          'HOSTID' : 'ANY',
-          'HOSTPORT': 5053
-        }
         expected_floating_license_content = 'HOST localhost ANY 5053'
 
         # Act
-        self.licReg.set_floating_license_info(new_floating_license_server_info)
-        self.licReg.register()
+        register_floating_license(self.module_path, 'localhost', 'ANY', 5053)
         
         # Verify
         file_path = '{}/zivavfx.lic'.format(self.module_path)
-        self.assertTrue(os.path.exists(file_path))
-        self.assertTrue(os.path.isfile(file_path))
+        self.assertTrue(path.exists(file_path))
+        self.assertTrue(path.isfile(file_path))
         with open(file_path, 'r') as lic_file:
             generated_content = lic_file.read()
             self.assertEqual(expected_floating_license_content, generated_content)
-
-
-    def test_invalid_license_server_info(self):
-        self.licReg.set_floating_license_info({})
-        with self.assertRaises(RuntimeError):
-            self.licReg.register()
-
 
