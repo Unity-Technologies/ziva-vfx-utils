788834a1e3d2d491f633b0dfde6beb93e14cac67
Chengdong Gui <chengdongg@zivadynamics.com>
2019-10-29 18:09:27 -0700

    VFXACT-403 Create license info with input data
    This commit implements creating license info based on input data:
    license type, license file path(node-based), license server
    info(floating).
    It writes the content to module path, with fixed name, zivavfx.lic
    It handles the simplest case, following tasks will take merging, error
    handling during read/write operation.
    Unit test is added.

--- tests/CmtTests/tests/test_licenseRegister.py
+++ tests/CmtTests/tests/test_licenseRegister.py
@@ -1,20 +1,93 @@
+import unittest
+import os
 from vfx_test_case import VfxTestCase
 from os import path
+import re
+import tempfile
 import utils.licenseRegister.licenseRegister as licReg
 
 class TestLicenseRegister(VfxTestCase):
   
-  def setUp(self):
-    super(TestLicenseRegister, self).setUp()
-    plugin_path = path.abspath(__file__)
-    self.licReg = licReg.LicenseRegister(plugin_path)
+    def setUp(self):
+        super(TestLicenseRegister, self).setUp()
+
+        # setup module path
+        self.temp_dir = tempfile.gettempdir()
+        self.module_path = self.temp_dir + '/Ziva/VFX/Ziva-VFX-Maya-Module'
+        try:
+            os.mkdir(self.module_path)
+        except OSError:
+            pass  # path exists, proceed
+        
+        self.licReg = licReg.LicenseRegister(self.module_path)
+
+    def test_register_without_set_license_type(self):
+        with self.assertRaises(RuntimeError):
+            self.licReg.register()
 
 
-  def test_register_node_based_license(self):
-    self.licReg.set_node_based_license_info('ZivaVFX.lic')
-    self.assertTrue(self.licReg.register())
+    def test_register_node_based_license(self):
+        '''
+        Test registering a node-based license, no existing license file
+        '''
+        # Setup
+        new_node_based_license_content = '''
+LICENSE zivadyn ziva-vfx-author 1.99 1-jan-2020 uncounted hostid=ANY
+  _ck=abcde12345 sig="AABBCCDDEEFF112233445566AABBCCDDEEFF112233445566
+  ABCDEF123456ABCDEF123456ABCDEF123456"
+'''.strip()
+        new_node_based_license_file_path = \
+            '{}/new_node_based_license.lic'.format(self.temp_dir)
+        with open(new_node_based_license_file_path, 'w') as input_file:
+            input_file.write(new_node_based_license_content)
+            
+        # Act
+        self.licReg.set_node_based_license_info(new_node_based_license_file_path)
+        self.licReg.register()
+
+        # Verify
+        file_path = '{}/zivavfx.lic'.format(self.module_path)
+        self.assertTrue(os.path.exists(file_path))
+        self.assertTrue(os.path.isfile(file_path))
+        with open(file_path, 'r') as lic_file:
+            generated_content = lic_file.read()
+            self.assertEqual(new_node_based_license_content, generated_content)
+
+    def test_non_exist_input_node_based_file(self):
+        # Act
+        self.licReg.set_node_based_license_info("non/exist/file/path")
+        with self.assertRaises(RuntimeError):
+            self.licReg.register()
 
 
-  def test_register_floating_license(self):
-    self.licReg.set_floating_license_info({})
-    self.assertTrue(self.licReg.register())
+    def test_register_floating_license(self):
+        '''
+        Test registering a floating license, no existing license file
+        '''
+        # Setup
+        new_floating_license_server_info = {
+          'SERVER' : 'localhost',
+          'HOSTID' : 'ANY',
+          'HOSTPORT': 5053
+        }
+        expected_floating_license_content = 'HOST localhost ANY 5053'
+
+        # Act
+        self.licReg.set_floating_license_info(new_floating_license_server_info)
+        self.licReg.register()
+        
+        # Verify
+        file_path = '{}/zivavfx.lic'.format(self.module_path)
+        self.assertTrue(os.path.exists(file_path))
+        self.assertTrue(os.path.isfile(file_path))
+        with open(file_path, 'r') as lic_file:
+            generated_content = lic_file.read()
+            self.assertEqual(expected_floating_license_content, generated_content)
+
+
+    def test_invalid_license_server_info(self):
+        self.licReg.set_floating_license_info({})
+        with self.assertRaises(RuntimeError):
+            self.licReg.register()
+
+
--- utils/licenseRegister/licenseRegister.py
+++ utils/licenseRegister/licenseRegister.py
@@ -1,41 +1,78 @@
+import os.path as path
+
 class LicenseRegister(object):
 
-  # License type
-  NODE_BASED_LICENSE = 0
-  FLOATING_LICENSE = 1
+    # License type
+    NODE_BASED_LICENSE = 0
+    FLOATING_LICENSE = 1
 
-  def __init__(self, plugin_path):
-    super(LicenseRegister, self).__init__()
-    self.plugin_path = plugin_path
-    self.license_type = None
-    self.license_path = None # for Node-based license
-    self.license_server_info = {} # for Floating license
+    # This is the file name LicenseRegister reads, merges and writes.
+    # It does not override or conflict with existing license info in other .lic files,
+    # RLM enusres loading all .lic files and parse them in its search paths.
+    LICENSE_FILE_NAME = 'zivavfx.lic'
 
-
-  def set_node_based_license_info(self, license_path):
-    self.license_type = LicenseRegister.NODE_BASED_LICENSE
-    self.license_path = license_path
-
-
-  def set_floating_license_info(self, license_server_info):
-    self.license_type = LicenseRegister.FLOATING_LICENSE
-    self.license_server_info = license_server_info
+    def __init__(self, module_path):
+        super(LicenseRegister, self).__init__()
+        self.module_path = module_path
+        self.license_type = None
+        self.license_path = None # for Node-based license
+        self.license_server_info = {} # for Floating license
+        self.lic_file_content = {} # parse result of existing license file
 
 
-  def register(self):
-    if LicenseRegister.NODE_BASED_LICENSE == self.license_type:
-      self._register_node_based_license()
-    elif LicenseRegister.FLOATING_LICENSE == self.license_type:
-      self._register_floating_license()
-    else:
-      raise ValueError("Unknonwn license type to register: {}".format(self.license_type))
-    
-    return True
+    def set_node_based_license_info(self, license_path):
+        self.license_type = LicenseRegister.NODE_BASED_LICENSE
+        self.license_path = license_path
 
 
-  def _register_node_based_license(self):
-    pass
+    def set_floating_license_info(self, license_server_info):
+        self.license_type = LicenseRegister.FLOATING_LICENSE
+        self.license_server_info = license_server_info
 
 
-  def _register_floating_license(self):
-    pass
+    def register(self):
+        if LicenseRegister.NODE_BASED_LICENSE == self.license_type:
+          self._register_node_based_license()
+        elif LicenseRegister.FLOATING_LICENSE == self.license_type:
+          self._register_floating_license()
+        else:
+          raise RuntimeError("Unknonwn license type to register: {}".format(self.license_type))
+
+
+    def _register_node_based_license(self):
+        assert(LicenseRegister.NODE_BASED_LICENSE == self.license_type)
+
+        try:
+            content = self._read_license_content()
+        except:
+          raise RuntimeError("Fail to read input license file: {}".format(self.license_path))
+        
+        self._write_license_content(content)
+
+
+    def _register_floating_license(self):
+        assert(LicenseRegister.FLOATING_LICENSE == self.license_type)
+
+        try:
+            content = 'HOST {} {} {}'.format(
+                self.license_server_info['SERVER'],
+                self.license_server_info['HOSTID'],
+                self.license_server_info['HOSTPORT']
+            )
+        except:
+            raise RuntimeError("Invalid floating license server info: {}".
+                format(self.license_server_info))
+
+        self._write_license_content(content)
+
+
+    def _read_license_content(self):
+        with open(self.license_path, 'r') as lic_file:
+            content = lic_file.read()
+            return content
+
+
+    def _write_license_content(self, content):
+        output_path = '{}/{}'.format(self.module_path, LicenseRegister.LICENSE_FILE_NAME)
+        with open(output_path, 'w') as lic_file:
+            lic_file.write(content)
