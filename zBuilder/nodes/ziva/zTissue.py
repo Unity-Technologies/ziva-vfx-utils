c1599620495a703ac545c09e8d8b365ea736479f
Lonnie Kraatz <lonniek@zivadynamics.com>
2019-11-15 09:46:47 -0800

    VFXACT-409 Removing an extra loop when applying attrs to zTissue
    The applying of attributes was happening in an extra check if its first item.  Moved to same check as 'build_multiple'.  This happens here because it only needs to run once, AND it needs to run regardless if there are zTissues to build.  This case comes up when you copy/paste for example.  Any time you want to apply settings when the tissue is in scene already.

--- zBuilder/nodes/ziva/zTissue.py
+++ zBuilder/nodes/ziva/zTissue.py
@@ -12,6 +12,7 @@ class TissueNode(Ziva):
     """
     type = 'zTissue'
     """ The type of node. """
+
     def __init__(self, parent=None, builder=None):
         super(TissueNode, self).__init__(parent=parent, builder=builder)
         self.children_tissues = None
@@ -57,7 +58,7 @@ class TissueNode(Ziva):
         tissue_items = self.builder.get_scene_items(type_filter='zTissue', name_filter=name_filter)
         tet_items = self.builder.get_scene_items(type_filter='zTet', name_filter=name_filter)
 
-        if self is tissue_items[0]: 
+        if self is tissue_items[0]:
             build_multiple(tissue_items,
                            tet_items,
                            attr_filter=attr_filter,
@@ -65,8 +66,7 @@ class TissueNode(Ziva):
                            solver=solver,
                            interp_maps=interp_maps)
 
-        # we only want to execute this if tissue_items is empty or self is the first tissue.
-        if not tissue_items or self == tissue_items[0]:  ### VFXACT-409 SUSPICIOUS
+            # we only want to execute this if tissue_items is empty or self is the first tissue. 
             # set the attributes in maya
             for ztet, ztissue in zip(tet_items, tissue_items):
                 ztet.set_maya_attrs(attr_filter=attr_filter)
