9656be0a15a7817b7f75f6400f850ae9a24ff34d
SEGA <stanp@zivadynamics.com>
2020-01-08 12:39:24 -0800

    VFXACT-508 Raname test_zbuilder_mesh file to test_zbuilder_geo

--- tests/CmtTests/tests/test_zbuilder_geo.py
+++ tests/CmtTests/tests/test_zbuilder_geo.py
@@ -6,10 +6,10 @@ from zBuilder.nodes.dg_node import DGNode
 from vfx_test_case import VfxTestCase
 
 
-class ZivaMeshTestCase(VfxTestCase):
+class ZivaGeoTestCase(VfxTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.mesh_names = ['c_cloth_1',
+        cls.geo_names = ['c_cloth_1',
                           'l_tissue_1',
                           'c_tissue_3',
                           'l_bone_1',
@@ -18,10 +18,10 @@ class ZivaMeshTestCase(VfxTestCase):
                           'c_bone_2',
                           'r_tissue_2',
                           'l_loa_curve']
-        cls.mesh_attrs = []
+        cls.geo_attrs = []
 
     def setUp(self):
-        super(ZivaMeshTestCase, self).setUp()
+        super(ZivaGeoTestCase, self).setUp()
         test_utils.build_generic_scene()
         self.builder = zva.Ziva()
         self.builder.retrieve_from_scene()
@@ -29,9 +29,9 @@ class ZivaMeshTestCase(VfxTestCase):
     def tearDown(self):
         if os.path.exists(self.temp_file_path):
             os.remove(self.temp_file_path)
-        super(ZivaMeshTestCase, self).tearDown()
+        super(ZivaGeoTestCase, self).tearDown()
 
-    def check_retrieve_mesh_looks_good(self, builder):
+    def check_retrieve_geo_looks_good(self, builder):
         """Args:
             builder (builders.ziva.Ziva()): builder object
         """
@@ -39,12 +39,12 @@ class ZivaMeshTestCase(VfxTestCase):
         for node in nodes.values():
             self.assertTrue(hasattr(node, 'depends_on'))
             self.assertIsInstance(node, DGNode)
-        self.assertItemsEqual(self.mesh_names, [x.name for x in nodes.values()])
+        self.assertItemsEqual(self.geo_names, [x.name for x in nodes.values()])
 
     def test_retrieve(self):
-        self.check_retrieve_mesh_looks_good(self.builder)
+        self.check_retrieve_geo_looks_good(self.builder)
 
     def test_retrieve_connections(self):
         builder = zva.Ziva()
         builder.retrieve_connections()
-        self.check_retrieve_mesh_looks_good(builder)
+        self.check_retrieve_geo_looks_good(builder)
