8d0181997023b630842937e51885978318c4f39c
Lonnie Kraatz <lonniek@zivadynamics.com>
2020-01-30 13:33:46 -0800

    VFXACT-530 This can return nothing so need to check for that.

--- zBuilder/builder.py
+++ zBuilder/builder.py
@@ -300,7 +300,9 @@ def restore_scene_items_from_string(item, builder):
         for parm in item:
             item[parm] = builder.get_scene_items(name_filter=item[parm])
     else:
-        item = builder.get_scene_items(name_filter=item)[0]
+        item = builder.get_scene_items(name_filter=item)
+        if item:
+            item = item[0]
     return item
 
 
