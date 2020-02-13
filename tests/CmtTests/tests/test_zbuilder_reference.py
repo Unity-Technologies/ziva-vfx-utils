edc7ba9a969203d2c015205060d523a1775c9fa6
Lonnie Kraatz <lonniek@zivadynamics.com>
2020-02-13 14:23:13 -0800

    VFXACT-522 Added reference tests for mirroring

--- tests/CmtTests/tests/test_zbuilder_reference.py
+++ tests/CmtTests/tests/test_zbuilder_reference.py
@@ -47,3 +47,86 @@ class ZivaReferenceGenericTestCase(VfxTestCase):
 
         self.check_builder_nodes_built_in_scene(builder)
         self.check_builder_nodes_setattr_in_scene(builder)
+
+
+class ZivaReferenceMirrorTestCase(VfxTestCase):
+    """This Class tests a specific type of "mirroring" so there are some assumptions made
+
+    - geometry has an identifiable qualifier, in this case it is l_ and r_
+    - Both sides geometry are in the scene
+    - One side has Ziva VFX nodes and other side does not, in this case l_ has Ziva nodes
+
+    """
+
+    def setUp(self):
+        super(ZivaReferenceMirrorTestCase, self).setUp()
+        test_utils.reference_scene(scene_name='mirror_example.ma')
+        self.builder = zva.Ziva()
+        self.builder.retrieve_from_scene()
+
+        # gather info
+        self.types = [
+            'zTissue',
+            'zTet',
+            'zAttachment',
+            'zRivetToBone',
+            'zRestShape',
+            'zMaterial',
+            'zLineOfAction',
+            'zFiber',
+            'zBone',
+        ]
+        self.scene_items_retrieved = self.builder.get_scene_items(type_filter=self.types)
+        self.l_item_geo = [
+            x for x in self.scene_items_retrieved if x.association[0].startswith('l_')
+        ]
+
+    def check_retrieve_ztissue_looks_good(self, builder, expected_plugs):
+        """Args:
+            builder (builders.ziva.Ziva()): builder object
+            expected_plugs (dict): A dict of expected attribute/value pairs.
+                                   {'zTissue1.collisions':True, ...}.
+                                   If None/empty/False, then attributes are taken from zBuilder
+                                   and values are taken from the scene.
+                                   Test fails if zBuilder is missing any of the keys
+                                   or has any keys with different values.
+        """
+        item_names = [x.name for x in self.scene_items_retrieved]
+        for type_ in self.types:
+            self.check_retrieve_looks_good(builder, expected_plugs, item_names, type_)
+
+    def test_builder_change_with_string_replace(self):
+        ## VERIFY
+
+        # find left and right tissue items regardless of name, by looking at mesh they
+        # are tied to
+        r_item_geo = [x for x in self.scene_items_retrieved if x.association[0].startswith('r_')]
+        self.assertEqual(len(self.l_item_geo), 0)  # Left geo should have been all renamerd to r_
+        self.assertEqual(r_item_geo, [])  # Make sure no r_ geo is in original scene
+
+        ## ACT
+        self.builder.string_replace("^l_", "r_")
+
+        ## VERIFY
+        new_left_geo = [x for x in self.scene_items_retrieved if x.association[0].startswith('l_')]
+        r_item_geo = [x for x in self.scene_items_retrieved if x.association[0].startswith('r_')]
+        self.assertEqual(len(self.l_item_geo),
+                         len(r_item_geo))  # number of right geos equal original left
+        self.assertEqual(new_left_geo, [])  # after replace left geo should have been renamed
+
+    def test_builder_build_with_string_replace(self):
+        # ACT
+        self.builder.string_replace("^l_", "r_")
+        self.builder.build()
+
+        # VERIFY
+        item_names_in_builder = [x.name for x in self.scene_items_retrieved]
+        # Original Ziva nodes should still be in scene
+        self.assertSceneHasNodes(item_names_in_builder)
+
+        # comparing attribute values between builder and scene
+        for scene_item in self.scene_items_retrieved:
+            scene_name = scene_item.name
+            for attr in scene_item.attrs.keys():
+                scene_value = cmds.getAttr('{}.{}'.format(scene_name, attr))
+                self.assertTrue(scene_value == scene_item.attrs[attr]['value'])
