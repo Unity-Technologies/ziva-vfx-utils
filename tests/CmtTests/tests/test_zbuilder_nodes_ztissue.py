ae76b527cae19ac806e6ec68e262214ca7818b4e
SEGA <stanp@zivadynamics.com>
2020-02-25 14:54:38 -0800

    VFXACT-622 Add zTissue duplicate tests

--- tests/CmtTests/tests/test_zbuilder_nodes_ztissue.py
+++ tests/CmtTests/tests/test_zbuilder_nodes_ztissue.py
@@ -113,3 +113,59 @@ class ZivaTissueGenericTestCase(VfxTestCase):
 
         ## VERIFY
         self.assertEqual(len(cmds.ls("r_tissue_1_zTissue")), 1)
+
+
+class ZivaTissueDuplicateTestCase(VfxTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.tissue_names = [
+            "l_tissue_1_zTissue", "r_tissue_2_zTissue", "c_tissue_3_zTissue",
+            "r_subtissue_1_zTissue"
+        ]
+        cls.tissue_attrs = ["inertialDamping", "pressureEnvelope", "collisions"]
+
+    def setUp(self):
+        super(ZivaTissueDuplicateTestCase, self).setUp()
+        test_utils.load_scene(scene_name="generic_tissue_duplicate.ma")
+        cmds.select("group1|zSolver1")
+        self.builder = zva.Ziva()
+        self.builder.retrieve_from_scene()
+        cmds.ziva("group1|zSolver1", defaultSolver=True)
+
+    def check_retrieve_ztissue_looks_good(self, builder, expected_plugs):
+        """Args:
+            builder (builders.ziva.Ziva()): builder object
+            expected_plugs (dict): A dict of expected attribute/value pairs.
+                                   {'zTissue1.collisions':True, ...}.
+                                   If None/empty/False, then attributes are taken from zBuilder
+                                   and values are taken from the scene.
+                                   Test fails if zBuilder is missing any of the keys
+                                   or has any keys with different values.
+        """
+        self.check_retrieve_looks_good(builder, expected_plugs, self.tissue_names, "zTissue")
+
+    def test_retrieve(self):
+        self.check_retrieve_ztissue_looks_good(self.builder, {})
+
+    def test_retrieve_connections(self):
+        builder = zva.Ziva()
+        builder.retrieve_connections()
+        self.check_retrieve_ztissue_looks_good(builder, {})
+
+    def test_build_restores_attr_values(self):
+        self.check_build_restores_attr_values(self.builder, self.tissue_names, self.tissue_attrs)
+
+    def test_remove(self):
+        self.check_ziva_remove_command(self.builder, "zTissue")
+
+    def test_builder_has_same_tissue_nodes_after_writing_to_disk(self):
+        builder = self.get_builder_after_writing_and_reading_from_disk(self.builder)
+        self.check_retrieve_ztissue_looks_good(builder, {})
+
+    def test_build(self):
+        builder = self.get_builder_after_clean_and_build(self.builder)
+        self.check_retrieve_ztissue_looks_good(builder, {})
+
+    def test_build_from_file(self):
+        builder = self.get_builder_after_write_and_retrieve_from_file(self.builder)
+        self.check_retrieve_ztissue_looks_good(builder, {})
