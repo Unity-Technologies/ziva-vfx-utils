0f6bce3464ecb6634b042a8fcc08f253df56dcff
Chengdong Gui <chengdongg@zivadynamics.com>
2020-10-16 10:13:05 -0700

    VFXACT-196 Update CI scripts

---
+++ tests/CmtTests/bitbucket-notify.py
@@ -0,0 +1,31 @@
+import sys      
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+project = sys.argv[1]
+build_state = sys.argv[2]
+commit_hash = sys.argv[3]
+build_no = sys.argv[4]
+build_url = sys.argv[5]
+
+usr = sys.argv[6] # User for Bitbucket
+pws = sys.argv[7] # Password for Bitbucket
+
+bitbucket_url = 'https://api.bitbucket.org/2.0/repositories/zivadynamics/ziva-vfx-utils/commit/'  + commit_hash + '/statuses/build'
+headers = {'Content-Type': 'application/json'}
+
+datas = {
+    "state": build_state,
+	"key": "{0}-AWS".format(project),
+	"name": "{0} build in AWS".format(project),
+	"url": build_url,
+	"description": "{0} build in AWS #{1}".format(project, build_no)
+}
+
+r = requests.post(bitbucket_url, json=datas, auth=HTTPBasicAuth(usr, pws), headers=headers)
+# Response code 201 - new status added, 200 - existing status updated.
+if r.status_code == 200 or r.status_code == 201:
+    print("{0} build in AWS #{1} set {2}".format(project, build_no, build_state))
+else:
+    print("Bitbucket API error - {0}".format(r.status_code))
---
+++ tests/CmtTests/copy_plugin_zBuilder_tests.py
@@ -0,0 +1,42 @@
+# Copy plugin for zBuilder tests
+# with specified maya version as a first argument ( default is 2018 )
+# Usage: python  tests/CmtTests/copy_plugin_zBuilder_tests.py --maya 2018
+
+import os
+import sys
+import subprocess
+import argparse
+import json
+
+current_directory_path = os.path.dirname(os.path.realpath(__file__))
+
+parser = argparse.ArgumentParser(description='Runs unit tests for a Maya module')
+parser.add_argument('--maya',
+                    help='Maya version',
+                    default='2018')
+
+pargs = parser.parse_args()
+
+cmd = ['aws s3 cp']
+
+with open('{0}/settings.json'.format(current_directory_path)) as json_file:
+    data = json.load(json_file)
+
+if sys.platform.startswith('linux'):
+    maya_plugin_version = 'lin_' + pargs.maya
+    maya_plugin_s3_path = 's3://ziva-ci/{0}'.format(data['plugin_path'][maya_plugin_version][13:])
+elif sys.platform.startswith('win32'):
+    maya_plugin_version = 'win_' + pargs.maya
+    maya_plugin_s3_path = 's3://ziva-ci/{0}'.format(data['plugin_path'][maya_plugin_version][3:])
+else:
+    raise Exception('OS {0} is not supported.'.format(sys.platform))
+
+if maya_plugin_version in data['plugin_path']:
+    cmd.extend([maya_plugin_s3_path])
+    cmd.extend(['{0}'.format(data['plugin_path'][maya_plugin_version])])
+else:
+    raise Exception('Plugin {0} is not listed. Please change settings.json.'.format(maya_plugin_version))
+
+exit_code = subprocess.check_call(' '.join(cmd) + ' --quiet', shell=True)
+
+sys.exit(exit_code)
