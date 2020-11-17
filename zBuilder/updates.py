82adfaf10293200ab5d20b55e8e4f49a70da70bf
Lonnie Kraatz <lonniek@zivadynamics.com>
2020-11-16 16:10:54 -0800

    VFXACT-764 Adding line of action rivet and rest shapes to update

--- zBuilder/updates.py
+++ zBuilder/updates.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import zBuilder.builder
 # def update_builder_pre_1_0_11(builder):
 #     """This updates any zBuilder file saved before 1.0.11.  It adds a reference for the
 #     parameters and changes an attribute name.  This change happened during 1.9 of Ziva VFX.
@@ -35,11 +36,23 @@ from collections import defaultdict
 def update_json_pre_1_0_11(json_object):
     type_ = json_object.get('type', 'Base')
     builder_type = json_object['_builder_type']
-    obj = find_class(builder_type, type_)
+    obj = zBuilder.builder.find_class(builder_type, type_)
     if type_ in ['zMaterial', 'zTet', 'zFiber', 'zAttachment']:
         json_object['parameters'] = defaultdict(list)
         map_names = construct_map_names(json_object['_name'], obj.MAP_LIST)
         json_object['parameters']['map'].extend(map_names)
+
+    # pre 1.0.11 the attribute was called fiber on a zLineOfAction.
+    # As of 1.0.11 it is called fiber_item
+    if type_ == 'zLineOfAction':
+        json_object['fiber_item'] = json_object['fiber']
+        json_object.pop('fiber', None)
+    if type_ == 'zRivetToBone':
+        json_object['rivet_locator'] = []
+        json_object['rivet_locator_parent'] = []
+    if type_ == 'zRestShape':
+        json_object['tissue_item'] = json_object['tissue_name']
+        json_object.pop('tissue_name', None)
 
 
 def construct_map_names(name, map_list):
