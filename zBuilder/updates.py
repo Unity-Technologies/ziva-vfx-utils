5b80af49b17b009ebec583d863b59f439f18b003
Lonnie Kraatz <lonniek@zivadynamics.com>
2020-11-20 11:47:31 -0800

    VFXACT-764 Adding method to get the node types that are using a map
    This is so when retrieving a json from disk we can easily check which ones have maps.

--- zBuilder/updates.py
+++ zBuilder/updates.py
@@ -2,16 +2,27 @@ from collections import defaultdict
 import zBuilder.builder
 
 from zBuilder.nodes.deformer import construct_map_names
+from zBuilder.builder import get_node_types_with_maps
+import zBuilder.zMaya as mz
 
 
 def update_json_pre_1_0_11(json_object):
     type_ = json_object.get('type', 'Base')
     builder_type = json_object['_builder_type']
     obj = zBuilder.builder.find_class(builder_type, type_)
-    if type_ in ['zMaterial', 'zTet', 'zFiber', 'zAttachment']:
+
+    if type_ in get_node_types_with_maps():
         json_object['parameters'] = defaultdict(list)
         map_names = construct_map_names(json_object['_name'], obj.MAP_LIST)
         json_object['parameters']['map'].extend(map_names)
+
+        # the association holds the mesh name associated with node.  Currently the only
+        # use for meshes in zBuilder is for interpolating weight maps if needed.
+        # this means that a mesh is only stored for a node if it has a map.
+
+        # first we need to use short name for mesh
+        meshes = [x.split('|')[-1] for x in json_object['_association']]
+        json_object['parameters']['mesh'] = meshes
 
     # pre 1.0.11 the attribute was called fiber on a zLineOfAction.
     # As of 1.0.11 it is called fiber_item
