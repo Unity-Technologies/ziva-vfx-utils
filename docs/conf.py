731f11058a713a3ca564f206f5e2431d2ab1cef9
Crawford Doran <crawfordd@zivadynamics.com>
2021-07-16 11:17:25 -0700

    Bugfix: Typo in name of logo file

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_logo = "images/Ziva-Logo-Large_NEWColor.png"
+html_logo = "images/Ziva-Logo-Large_NEWcolor.png"
 
 #html_theme = 'classic'
 
