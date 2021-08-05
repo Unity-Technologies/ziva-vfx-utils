de9c5ab544c135c38c71fdc35394eaa673098d4b
alexander_u <alexu@zivadynamics.com>
2021-08-05 12:54:15 -0700

    VFXACT-1011 Created ScenePanel2MenuBar class and use it to hold menu items for the new scene panel

---
+++ zBuilder/scenePanel2/menuBar.py
@@ -0,0 +1,18 @@
+from PySide2 import QtWidgets, QtGui, QtCore
+
+class ScenePanel2MenuBar(QtWidgets.QMenuBar):
+    def __init__(self, parent=None):
+        super(ScenePanel2MenuBar, self).__init__(parent)
+
+        self.setup_menu_items()
+
+    def setup_menu_items(self):
+        self.file_menu = self.addMenu("File")
+        self.cache_menu = self.addMenu("Cache")
+        self.tool_menu = self.addMenu("Tool")
+        self.help_menu = self.addMenu("Help")
+
+        self.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+       
+   
+
