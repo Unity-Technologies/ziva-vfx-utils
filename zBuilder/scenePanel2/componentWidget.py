fd8bcc92011e247572708f970b51778437b63385
alexander_u <alexu@zivadynamics.com>
2021-08-20 15:15:54 -0700

    VFXACT-1037 In this commit I've added popup menus for zMaterial and zAttachment nodes to the new scene panel

--- zBuilder/scenePanel2/componentWidget.py
+++ zBuilder/scenePanel2/componentWidget.py
@@ -4,7 +4,7 @@ from ..uiUtils import nodeRole, get_icon_path_from_name, get_icon_path_from_node
 from .zTreeView import zTreeView
 from .treeItem import TreeItem, build_scene_panel_tree
 from PySide2 import QtCore, QtGui, QtWidgets
-from maya import cmds
+from maya import cmds, mel
 from collections import defaultdict
 from functools import partial
 from zBuilder.nodes.base import Base
@@ -95,6 +95,81 @@ def paste_weights(node, new_map_index):
     if dialog_return == QtWidgets.QMessageBox.Yes or orig_map_length == new_map_length:
         new_map.copy_values_from(orig_map)
         new_map.apply_weights()
+
+class ProximityWidget(QtWidgets.QWidget):
+    """
+    Widget in right-click menu to change map weights for attachments
+    """
+    def __init__(self, parent=None):
+        super(ProximityWidget, self).__init__(parent)
+        h_layout = QtWidgets.QHBoxLayout(self)
+        h_layout.setContentsMargins(15, 15, 15, 15)
+        self.from_edit = MenuLineEdit()
+        self.from_edit.setFixedHeight(24)
+        self.from_edit.setPlaceholderText("From")
+        self.from_edit.setText("0.1")
+        self.from_edit.setFixedWidth(40)
+        self.to_edit = MenuLineEdit()
+        self.to_edit.setFixedHeight(24)
+        self.to_edit.setPlaceholderText("To")
+        self.to_edit.setText("0.2")
+        self.to_edit.setFixedWidth(40)
+        ok_button = QtWidgets.QPushButton()
+        ok_button.setText("Ok")
+        h_layout.addWidget(self.from_edit)
+        h_layout.addWidget(self.to_edit)
+        h_layout.addWidget(ok_button)
+        ok_button.clicked.connect(self.paint_by_prox)
+        # setTabOrder doesn't work when used for menu
+        # need to use next 2 lines as a workaround
+        self.setFocusProxy(self.to_edit)
+        ok_button.setFocusProxy(self.from_edit)
+        self.from_edit.acceptSignal.connect(self.paint_by_prox)
+        self.to_edit.acceptSignal.connect(self.paint_by_prox)
+
+    def paint_by_prox(self):
+        """Paints attachment map by proximity.
+        """
+        # to_edit can't have smaller value then from_edit
+        from_value = float(self.from_edit.text())
+        to_value = float(self.to_edit.text())
+        if to_value < from_value:
+            self.to_edit.setText(str(from_value))
+        mel.eval('zPaintAttachmentsByProximity -min {} -max {}'.format(
+            self.from_edit.text(), self.to_edit.text()))
+
+class MenuLineEdit(QtWidgets.QLineEdit):
+    """
+    Groups LineEdits together so after you press Tab it switch focus to sibling_right.
+    If Shift+Tab pressed it uses sibling_left.
+    Sends acceptSignal when Enter or Return button is pressed.
+    This is for use in Menus, where tab navigation is broken out of the box,
+    and the 'entered pressed' action undesirably causes the menu to close sometimes.
+    """
+    acceptSignal = QtCore.Signal()
+
+    def __init__(self, parent=None):
+        super(MenuLineEdit, self).__init__(parent)
+
+    def event(self, event):
+        if event.type() == QtCore.QEvent.KeyPress and event.key() == QtCore.Qt.Key_Tab:
+            self.nextInFocusChain().setFocus()
+            return True
+        if event.type() == QtCore.QEvent.KeyPress and event.modifiers() == QtCore.Qt.ShiftModifier:
+            # PySide bug, have to use this number instead of Key_Tab with modifiers
+            if event.key() == 16777218:
+                self.previousInFocusChain().setFocus()
+                return True
+
+        if event.type() == QtCore.QEvent.KeyPress and event.key() in [
+                QtCore.Qt.Key_Enter, QtCore.Qt.Key_Return
+        ]:
+            self.acceptSignal.emit()
+            # This will prevent menu to close after Enter/Return is pressed
+            return True
+
+        return super(MenuLineEdit, self).event(event)
+
 
 class ComponentTreeModel(QtCore.QAbstractItemModel):
     """ Tree model for each component
@@ -208,6 +283,8 @@ class ComponentSectionWidget(QtWidgets.QWidget):
         self._tvComponent.selectionModel().selectionChanged.connect(
             self.on_tvComponent_selectionChanged)
 
+        self._setup_actions()
+
     def open_menu(self, position):
         indexes = self._tvComponent.selectedIndexes()
 
@@ -220,7 +297,9 @@ class ComponentSectionWidget(QtWidgets.QWidget):
             'zLineOfAction': self.open_line_of_action_menu,
             'zRestShape': self.open_rest_shape_menu,
             'zTet': self.open_tet_menu,
-            'zFiber': self.open_fiber_menu
+            'zFiber': self.open_fiber_menu,
+            'zMaterial': self.open_material_menu,
+            'zAttachment': self.open_attachment_menu,
             }
 
         node = indexes[0].data(nodeRole)
@@ -336,6 +415,50 @@ class ComponentSectionWidget(QtWidgets.QWidget):
         end_points_map_menu = menu.addMenu('EndPoints')
         self.add_map_actions_to_menu(end_points_map_menu, node, 1)
 
+    def open_material_menu(self, menu, node):
+        self.add_attribute_actions_to_menu(menu, node)
+        menu.addSection('Maps')
+        weight_map_menu = menu.addMenu('Weight')
+
+        self.add_map_actions_to_menu(weight_map_menu, node, 0)
+
+    def select_source_and_target(self):
+        """
+        Selects the source and target mesh of an attachment. This is a menu command.
+        """
+        indexes = self._tvComponent.selectedIndexes()[0]
+        node = indexes.data(nodeRole)
+        cmds.select(node.nice_association)
+
+    def _setup_actions(self):
+        self.actionSelectST = QtWidgets.QAction(self)
+        self.actionSelectST.setText('Select Source and Target')
+        self.actionSelectST.setObjectName("actionSelectST")
+        self.actionSelectST.triggered.connect(self.select_source_and_target)
+
+    def open_attachment_menu(self, menu, node):
+        source_mesh_name = node.association[0]
+        target_mesh_name = node.association[1]
+
+        self.add_attribute_actions_to_menu(menu, node)
+        menu.addAction(self.actionSelectST)
+        menu.addSection('Maps')
+        truncate = lambda x: (x[:12] + '..') if len(x) > 14 else x
+        source_menu_text = 'Source ({})'.format(truncate(source_mesh_name))
+        target_menu_text = 'Target ({})'.format(truncate(target_mesh_name))
+
+        source_map_menu = menu.addMenu(source_menu_text)
+        self.add_map_actions_to_menu(source_map_menu, node, 0)
+
+        target_map_menu = menu.addMenu(target_menu_text)
+        self.add_map_actions_to_menu(target_map_menu, node, 1)
+
+        menu.addSection('')
+        proximity_menu = menu.addMenu('Paint By Proximity')
+        prox_widget = ProximityWidget()
+        action_paint_by_prox = QtWidgets.QWidgetAction(proximity_menu)
+        action_paint_by_prox.setDefaultWidget(prox_widget)
+        proximity_menu.addAction(action_paint_by_prox)
 
 
 class ComponentWidget(QtWidgets.QWidget):
