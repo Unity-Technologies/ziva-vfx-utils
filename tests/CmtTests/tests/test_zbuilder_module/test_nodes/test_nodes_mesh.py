7e1a3758ae0853d59235558765d33ebd82f6036f
Lonnie Kraatz <lonnie.kraatz@unity3d.com>
2023-01-18 22:04:06 -0800

    VFXACT-1590 change sphere to cube for test and put it all in +

--- tests/CmtTests/tests/test_zbuilder_module/test_nodes/test_nodes_mesh.py
+++ tests/CmtTests/tests/test_zbuilder_module/test_nodes/test_nodes_mesh.py
@@ -8,37 +8,37 @@ class ZivaMeshTestCase(VfxTestCase):
 
     def setUp(self):
         super(ZivaMeshTestCase, self).setUp()
-        cmds.polySphere(name='ball')
+        cmds.polyCube(n='cube')
+        cmds.move(1, 1, -1, 'cube')
         cmds.ziva(t=True)
-        results = cmds.ziva(f=True)
         self.builder = zva.Ziva()
         self.builder.retrieve_from_scene()
 
     def test_mesh_mirror_assert(self):
-        ball = self.builder.get_scene_items(name_filter='ball')[0]
+        ball = self.builder.get_scene_items(name_filter='cube')[0]
 
         with self.assertRaises(Exception):
             ball.mirror(mirror_axis='DD')
 
     def test_mesh_mirror_axist(self):
-        ball = self.builder.get_scene_items(name_filter='ball')[0]
+        ball = self.builder.get_scene_items(name_filter='cube')[0]
 
-        base_position = cmds.xform('ball.vtx[237]', q=True, ws=True, t=True)
+        base_position = cmds.xform('cube.vtx[0]', q=True, ws=True, t=True)
 
         ball.mirror(mirror_axis='X')
         mesh = ball.build_mesh()
-        new_position = cmds.xform(mesh + '.vtx[237]', q=True, ws=True, t=True)
-        self.assertEqual(base_position[0], -new_position[0])
+        new_position = cmds.xform(mesh + '.vtx[0]', q=True, ws=True, t=True)
+        self.assertApproxEqual(base_position[0], -new_position[0])
         cmds.delete(mesh)
 
         ball.mirror(mirror_axis='Y')
         mesh = ball.build_mesh()
-        new_position = cmds.xform(mesh + '.vtx[237]', q=True, ws=True, t=True)
-        self.assertEqual(base_position[1], -new_position[1])
+        new_position = cmds.xform(mesh + '.vtx[0]', q=True, ws=True, t=True)
+        self.assertApproxEqual(base_position[1], -new_position[1])
         cmds.delete(mesh)
 
         ball.mirror(mirror_axis='Z')
         mesh = ball.build_mesh()
-        new_position = cmds.xform(mesh + '.vtx[237]', q=True, ws=True, t=True)
-        self.assertEqual(base_position[2], -new_position[2])
+        new_position = cmds.xform(mesh + '.vtx[0]', q=True, ws=True, t=True)
+        self.assertApproxEqual(base_position[2], -new_position[2])
         cmds.delete(mesh)
